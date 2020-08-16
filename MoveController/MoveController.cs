ae3bd031b6ea75e3090f03847bc525625de308a7
SavrunE <igor1996igor@inbox.ru>
2020-08-16 14:39:18 +0300

    Move controller types

---
+++ MoveController/MoveController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private Vector3 direction;
+
+    void Start()
+    {
+        
+    }
+    //"Писать код в апдейте не лучая идея", тк не стабильно.
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime);
+    }
+}
