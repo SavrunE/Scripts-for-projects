71abab4d0dc4014d95985ed5954007249006cdec
SavrunE <69328877+SavrunE@users.noreply.github.com>
2020-08-22 18:10:10 +0300

    Create CameraMover3D.cs

---
+++ MoveController/CameraMover3D.cs
@@ -0,0 +1,15 @@
+//закрпим камеру за персонажем на координатах CameraPosition
+public class CameraMover : MonoBehaviour
+{
+    public GameObject PlayerPosition;
+    public Vector3 CameraPosition = new Vector3(0,4,-4);
+    void Start()
+    {
+        CameraPosition -= PlayerPosition.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = PlayerPosition.transform.position + CameraPosition;
+    }
+}
