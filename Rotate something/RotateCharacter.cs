5dd00c3b02d6886394802f573685c1d8e7b177da
SavrunE <69328877+SavrunE@users.noreply.github.com>
2020-09-04 15:01:42 +0300

    Update RotateCharacter.cs

--- Rotate something/RotateCharacter.cs
+++ Rotate something/RotateCharacter.cs
@@ -4,7 +4,7 @@ public bool LookOnLeft;
 private void Update()
 {
  float HorizontalInput = Input.GetAxis("Horizontal");
- if (LookOnLeft == true && HorizontalInput <= 0)
+ if (LookOnLeft == true && HorizontalInput < 0)
      Flip();
 else if (LookOnLeft == false && HorizontalInput > 0)
       Flip();
