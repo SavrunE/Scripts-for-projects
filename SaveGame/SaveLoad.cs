5ebe18c7f5a1d0a4a2c5687689c61b47c204e91c
SavrunE <69328877+SavrunE@users.noreply.github.com>
2020-11-25 19:51:22 +0300

    Update and rename save.cs to SaveLoad.cs

--- SaveGame/SaveLoad.cs
+++ SaveGame/SaveLoad.cs
@@ -5,13 +5,24 @@ public class GameHandler : MonoBehaviour
 {
     private void Start()
     {
+        LoadGame();
+    }
+    private void SaveGame()
+    {
         PlayerData playerData = new PlayerData();
         playerData.Viewers = 80;
         playerData.Money = 100;
         
         string json = JsonUtility.ToJson(playerData);
         File.WriteAllText(Application.dataPath + "/saveFile.json", json);
+    } 
+    private void LoadGame()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
+        PlayerData loadData = JsonUtility.FromJson<PlayerData>(json);
+        Debug.Log(loadData.Money);
     }
+
     class PlayerData
     {
         public int Viewers;
