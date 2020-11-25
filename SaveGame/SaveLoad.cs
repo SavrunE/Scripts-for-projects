b16c8f5ab59290bcf813793a38ee6603ef77d1e6
SavrunE <69328877+SavrunE@users.noreply.github.com>
2020-11-25 19:54:28 +0300

    Update SaveLoad.cs

--- SaveGame/SaveLoad.cs
+++ SaveGame/SaveLoad.cs
@@ -5,13 +5,14 @@ public class GameHandler : MonoBehaviour
 {
     private void Start()
     {
+        SaveGame(10,20);
         LoadGame();
     }
-    private void SaveGame()
+    private void SaveGame(int viewers, int money)
     {
         PlayerData playerData = new PlayerData();
-        playerData.Viewers = 80;
-        playerData.Money = 100;
+        playerData.Viewers = viewers;
+        playerData.Money = money;
         
         string json = JsonUtility.ToJson(playerData);
         File.WriteAllText(Application.dataPath + "/saveFile.json", json);
@@ -21,6 +22,7 @@ public class GameHandler : MonoBehaviour
         string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
         PlayerData loadData = JsonUtility.FromJson<PlayerData>(json);
         Debug.Log(loadData.Money);
+        Debug.Log(loadData.Viewers);
     }
 
     class PlayerData
