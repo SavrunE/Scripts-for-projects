df277ca030e5c561150b6debb4261a8c9d401959
SavrunE <69328877+SavrunE@users.noreply.github.com>
2020-12-17 15:39:18 +0300

    Create body.MovePosition.cs

---
+++ MoveController/body.MovePosition.cs
@@ -0,0 +1,3 @@
+        body.MovePosition(pathCreator.path.GetPointAtDistance(distanceTravelled));
+       // При использовании данного метода, можем переместить куда-то наш объект,
+        при этом, к нему примениться физическое взаимодействие.
